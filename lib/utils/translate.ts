45660b873e68050189eb49491ae6921ce887f886
Abdelhamid Boudjit <qhamid@proton.me>
2024-08-29 16:48:05 +0100

    create translate utlis

---
+++ lib/utils/translate.ts
@@ -0,0 +1,25 @@
+import translate, { googleTranslateApi } from "google-translate-api-x";
+import { JSONObject, extractStringsFromJSON, mapStringsToJSON } from "./json";
+
+type TranslateFunction = (
+  strings: string[],
+  language: string,
+) => Promise<googleTranslateApi.TranslationResponse[]>;
+
+export async function translateJSON(
+  json: JSONObject,
+  language: string,
+  translateFunction: TranslateFunction = translateStrings,
+): Promise<JSONObject> {
+  const { strings, paths } = extractStringsFromJSON(json);
+  const translations = await translateFunction(strings, language).then(
+    (res) => {
+      return res.map((tr) => tr.text);
+    },
+  );
+  return mapStringsToJSON(json, translations, paths);
+}
+
+export async function translateStrings(data: string[], lang: string) {
+  return await translate(data, { to: lang });
+}

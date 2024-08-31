d07d0ee9f7047a827b52c2e886d229a02f216eed
Abdelhamid Boudjit <qhamid@proton.me>
2024-08-31 14:31:29 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,19 @@
+"use client";
 import JSONEditor from "@/components/JSONEditor";
+import { translateJSON } from "@/lib/utils";
 
 export default function Home() {
+  const handleChange = async (e: string) => {
+    console.log(e);
+    // const t = await translateJSON(JSON.parse(e), "ar");
+    const t = await fetch("https://google.com");
+    console.log(t);
+  };
   return (
     <main className="h-full flex flex-col">
       <h1 className="text-2xl font-bold mb-4">JSON Editor</h1>
       <div className="flex h-full">
-        <JSONEditor />
+        <JSONEditor onChange={handleChange} />
         <JSONEditor immutable />
       </div>
     </main>

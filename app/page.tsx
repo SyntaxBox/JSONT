15465d0f2951ec8cdce8593a6dd850cef0197724
Abdelhamid Boudjit <qhamid@proton.me>
2024-09-08 18:54:21 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,31 @@
 "use client";
 import JSONEditor from "@/components/JSONEditor";
-import { translateJSON } from "@/lib/utils";
+import { debounce } from "lodash";
+import { useCallback } from "react";
 
 export default function Home() {
   const handleChange = async (e: string) => {
-    console.log(e);
-    // const t = await translateJSON(JSON.parse(e), "ar");
-    const t = await fetch("https://google.com");
-    console.log(t);
+    const res = await fetch("/api/translate", {
+      body: JSON.stringify({
+        json: e,
+        language: "ar",
+      }),
+      method: "POST",
+    });
+    console.log(await res.json());
   };
+
+  // Use useCallback to memoize the debounced version of handleChange
+  const debouncedHandleChange = useCallback(
+    debounce(handleChange, 500), // 500ms delay
+    [],
+  );
+
   return (
     <main className="h-full flex flex-col">
       <h1 className="text-2xl font-bold mb-4">JSON Editor</h1>
       <div className="flex h-full">
-        <JSONEditor onChange={handleChange} />
+        <JSONEditor onChange={debouncedHandleChange} />
         <JSONEditor immutable />
       </div>
     </main>

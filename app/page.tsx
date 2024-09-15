40cb358fed9b895615cda776853d68585ed93874
Abdelhamid Boudjit <qhamid@proton.me>
2024-09-15 10:22:55 +0100

    fix json download

--- app/page.tsx
+++ app/page.tsx
@@ -46,7 +46,7 @@ export default function Home() {
     return () => {
       window.removeEventListener("keydown", handleKeyDown);
     };
-  }, [text]);
+  }, [translatedText]);
 
   return (
     <>
